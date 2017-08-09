316b8ebca788540c6a0228fc3b49b33cdce09474
coprseintube0 <mochilink@gmail.com>
2017-08-09 21:28:23 +0700

    bug fixes

--- Lab2_NN/Program(conv).cs
+++ Lab2_NN/Program(conv).cs
@@ -7,7 +7,6 @@ namespace Neuronets
     {
         static void Main(string[] args)
         {
-            int a;
             //Конвертер валют
             var nnConv = new Neuronet(new[] {1, 2, 1}, FunctionType.Linear);
                 //конструктор, задающий общую структуру сети
